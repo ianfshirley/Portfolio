6f4a4e65153d935f702e080ac7ba6beeeb4be316
ianfshirley <ianfshirley88@gmail.com>
2023-06-07 22:49:14 -0700

    created and added new favicon, but it is not showing in the browser tab

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel='icon' href='/favicon.ico' />
+      </head>
       <body className=''>
         <Header />
         {children}
