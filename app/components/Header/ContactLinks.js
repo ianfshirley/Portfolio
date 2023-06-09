697a788ae0dcefeab422d341bebb077df406b87b
ianfshirley <ianfshirley88@gmail.com>
2023-06-08 22:50:03 -0700

    added fontawesome icons for contact links

--- app/components/Header/ContactLinks.js
+++ app/components/Header/ContactLinks.js
@@ -1,19 +1,20 @@
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faEnvelope } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope as faEnvelopeSolid } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function ContactLinks() {
   return (
     <div className="p-10">
-      <nav className="rounded border-2">
+      <nav className="rounded border-2  flex flex-row">
         <a href='https://github.com/ianfshirley' target='_blank' rel='noopener noreferrer' className="p-2">
-          GitHub
+          <FontAwesomeIcon icon={faGithub} className="w-5 h-5" />
         </a>
         <a href='https://www.linkedin.com/in/ianfshirley/' target='_blank' rel='noopener noreferrer' className="p-2">
-          LinkedIn
+          <FontAwesomeIcon icon={faLinkedin} className="w-5 h-5" />
         </a>
         <a href='mailto:ianfshirley88@gmail.com' target='_blank' rel='noopener noreferrer' className="p-2">
-          Email
+          <FontAwesomeIcon icon={faEnvelopeSolid} className="w-5 h-5" />
         </a>
       </nav>
     </div>
