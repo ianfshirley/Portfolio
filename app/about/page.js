2a25ce8140163c94d5f58f33165b31a0e2a36cf6
ianfshirley <ianfshirley88@gmail.com>
2023-06-10 22:15:02 -0700

    more color changes

--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,7 @@
 
 export default function About() {
   return (
-    <main className="">
+    <main className="w-full mx-10 bg-army">
       <h1
         className='w-full p-10'
       >About Page</h1>
