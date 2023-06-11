8654c5f7166086ca4064486379f858626ce5b14f
ianfshirley <ianfshirley88@gmail.com>
2023-06-10 22:11:50 -0700

    quick fix

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero"
+import About from "./about/page"
+import Portfolio from "./portfolio/page"
 
 
 export default function Home() {
   return (
     <main className="w-screen">
       <Hero />
+      <Portfolio />
+      <About />
     </main>
   )
 }
