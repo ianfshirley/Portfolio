2b22e3d84abb316d4fe4a54ac29566580f940fe4
ianfshirley <ianfshirley88@gmail.com>
2023-06-15 16:01:22 -0700

    updated hero section

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -7,10 +7,10 @@ export default function Hero() {
     <div className="mt-12  md:mx-10">
       <div className="flex flex-col md:flex-row justify-evenly items-center mx-10">
         <div className=" md:mx-10">
-          <h1 className="mb-1 text-2xl font-bold leading-tight text-peach-500 sm:text-xl">Hi, my name is</h1>
-          <h2 className="mb-2 text-5xl font-bold leading-tight text-mist-50 sm:text-7xl">Ian F. Shirley</h2>
-          <h3 className="mb-3 text-2xl font-medium text-army sm:text-5xl">Headline goes here</h3>
-          <p className="max-w-md mt-5 mb-5 text-lg text-mist sm:text-xl text-justify">Summary goes here. Some stuff about what I'm doing, what I excel at and/or what I like. Bla bla lorem impsum whatever bs they always have in these things</p>
+          <h1 className="mb-1 text-2xl font-bold leading-tight text-peach-500 sm:text-xl">Hey there,</h1>
+          <h2 className="mb-2 text-5xl font-bold leading-tight text-mist-50 sm:text-7xl">My name's Ian.</h2>
+          <h3 className="mb-3 text-2xl font-medium text-army sm:text-5xl">Full-Stack Developer & UI Designer</h3>
+          <p className="max-w-md mt-5 mb-5 text-lg text-mist sm:text-xl ">I'm passionate about bringing more order, efficiency, kindness and beauty into this world.</p>
         </div>
         <Skills />
       </div>
