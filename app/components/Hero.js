65fdf0df5137b26a9e6d0c1b9513d5677ab2b23d
ianfshirley <ianfshirley88@gmail.com>
2023-06-15 16:07:45 -0700

    quick fix

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
           <h1 className="mb-1 text-2xl font-bold leading-tight text-peach-500 sm:text-xl">Hey there,</h1>
           <h2 className="mb-2 text-5xl font-bold leading-tight text-mist-50 sm:text-7xl">My name's Ian.</h2>
           <h3 className="mb-3 text-2xl font-medium text-army sm:text-5xl">Full-Stack Developer & UI Designer</h3>
-          <p className="max-w-md mt-5 mb-5 text-lg text-mist sm:text-xl ">I'm passionate about bringing more order, efficiency, kindness and beauty into this world.</p>
+          <p className="max-w-md mt-5 mb-5 text-lg text-peach sm:text-xl ">I'm passionate about bringing more order, efficiency, kindness and beauty into this world.</p>
         </div>
         <Skills />
       </div>
