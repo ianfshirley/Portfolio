66a45f1b7fe77e2039c2e6d370cb01ebcb248b25
ianfshirley <ianfshirley88@gmail.com>
2023-06-22 16:50:32 -0700

    refactored Hero.js

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -7,13 +7,18 @@ export default function Hero() {
     <div className="mt-24 mb-24 md:mx-10">
       <div className="flex flex-col md:flex-row justify-evenly items-center mx-10">
         <div className=" md:mx-10">
-          <h1 className="mb-1 text-2xl font-bold leading-tight text-peach-500 sm:text-xl">Hey there,</h1>
-          <h2 className="mb-2 text-5xl font-bold leading-tight text-mist-50 sm:text-7xl">My name's Ian.</h2>
-          <h3 className=" text-2xl font-medium text-army sm:text-5xl">Full-Stack Developer</h3>
+          <h1 className="mb-1 text-2xl font-bold leading-tight text-peach-500 sm:text-xl">
+            Hey there,
+          </h1>
+          <h2 className="mb-2 text-5xl font-bold leading-tight text-mist-50 sm:text-7xl">
+            My name's Ian.
+          </h2>
+          <h3 className=" text-2xl font-medium text-army sm:text-5xl">
+            Full-Stack Developer
+          </h3>
           <h5 className="mb-3 text-lg font-medium text-army sm:text-2xl">
             & UI Designer
           </h5>
-          {/* <p className="max-w-md mt-5 mb-5 text-lg text-peach sm:text-xl "></p> */}
         </div>
         <Skills />
       </div>
