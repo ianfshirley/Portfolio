0e5eb6c6397d53b06a470136c3d1045080acf50c
ianfshirley <ianfshirley88@gmail.com>
2023-06-22 16:54:17 -0700

    refactored layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
       </head>
       <body className='w-screen bg-[#191919] absolute'>
         <Header />
